ed4b8914bca26219f91d0947b85d287fcb4f4677
briceth <tessierhuort@gmail.com>
2022-06-09 12:08:44 +0200

    add test:ci cmd in package.json

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -15,6 +15,6 @@ it('clears the cookie after the signing out', async () => {
     .expect(200);
 
   expect(response.get('Set-Cookie')[0]).toBe(
-    'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
+    'express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
   );
 });

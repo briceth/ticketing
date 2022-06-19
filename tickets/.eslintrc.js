0bec611685ce50bc0ff6d0c1011245e9aeb0dca8
briceth <tessierhuort@gmail.com>
2022-06-19 16:19:29 +0200

    add husky and lint staged hook

--- tickets/.eslintrc.js
+++ tickets/.eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
